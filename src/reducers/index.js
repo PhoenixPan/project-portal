44ce614f4a6c5ddc63ab6de523a8a6350a3a8700
Phoenix Pan <phoenixpan1992@gmail.com>
2018-06-18 14:27:37 +1000

    test git

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import { reducer as formReducer } from 'redux-form'
 import GetAllUserDesignsReducer from './getAllUserDesignsReducer';
 import UserSignUpReducer from './userSignUpReducer';
 
-// setuo global state
 const rootReducer = combineReducers(
   { 
     form: formReducer,
